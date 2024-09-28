e12c6e5bb53cda7989f85d0c2e9d69d3a1c01911
Cristobal Varas <cristobal.varasp@gmail.com>
2024-09-28 19:23:19 -0300

    Agregando función 2. Buscar

--- Funciones.py
+++ Funciones.py
@@ -5,6 +5,15 @@ import re
 vehiculos = []
 
 
+def menu():
+    '''Definiendo la función que mostrará el menú'''
+    print("Menú Automotora Ruta 40 (Región de Los Lagos)")
+    print("1. Registrar vehículo")
+    print("2. Buscar vehículo")
+    print("3. Impresión de certificados")
+    print("4. Salir")
+
+
 def validar_tipo(tipo):
     '''Validar que el tipo de vehiculo ingresado sea uno de los 4 posibles'''
     tipo_vehiculo = ["Auto", "Suvs", "Pickups", "Vans"]
@@ -35,11 +44,11 @@ def validar_precio(precio):
     return precio >= 6880000
 
 
-def validar_fecha(fecha_registro):
+def validar_fecha(fecha):
     '''Validar que la fecha esté en el formato DD-MM-AAAA'''
     # Expresión regular para el formato DD-MM-AAAA
     patron = r'^\d{2}-\d{2}-\d{4}$'
-    if re.match(patron, fecha_registro):
+    if re.match(patron, fecha):
         return True
     return False
 
@@ -49,8 +58,11 @@ def validar_nom_duenno(nom_duenno):
 
 
 def validar_campo(mensaje, fx_validar, mensaje_error):
+    '''Función para validar un campo con opción de cancelar'''
     while True:
         valor = input(mensaje)
+        if valor == "00":
+            return "cancelar"
         if fx_validar(valor):
             return valor
         print(mensaje_error)
@@ -58,22 +70,115 @@ def validar_campo(mensaje, fx_validar, mensaje_error):
 
 def guardar(tipo, patente, marca, modelo, precio, multas, fecha_registro, nom_duenno):
     vehiculos.append({
-        "tipo": tipo,
-        "patente": patente,
-        "marca": marca,
-        "modelo": modelo,
+        "tipo": tipo.title(),
+        "patente": patente.upper(),
+        "marca": marca.title(),
+        "modelo": modelo.title(),
         "precio": precio,
         "multas": [
             {"monto": multa["monto"], "fecha": multa["fecha"]} for multa in multas],
         "fecha_registro": fecha_registro,
-        "nom_duenno": nom_duenno
+        "nom_duenno": nom_duenno.title()
     })
 
     print("Vehículo registrado exitosamente.")
 
 
+def registrar_vehiculo():
+    while True:
+        tipo = validar_campo(
+            "Ingrese el tipo de vehículo 'Auto', 'Suvs', 'Pickups', 'Vans' (00 para cancelar): ",
+            validar_tipo, "Tipo de vehículo inválido. Debe ser Auto, Suvs, Pickups o Vans.")
+        if tipo == "cancelar":
+            return
+
+        patente = validar_campo(
+            "Ingrese la patente del vehículo ('AA1111' o 'AAAA11') (00 para cancelar): ",
+            validar_patente, "Formato de patente inválido debe ser 'AA1111' o 'AAAA11'")
+        if patente == "cancelar":
+            return
+
+        marca = validar_campo(
+            "Ingrese la marca del vehículo (00 para cancelar): ",
+            validar_marca, "La marca debe contener entre 2 y 12 caracteres")
+        if marca == "cancelar":
+            return
+
+        modelo = validar_campo(
+            "Ingrese el modelo del vehículo (00 para cancelar): ",
+            validar_modelo, "No es posible dejar el campo vacío.")
+        if modelo == "cancelar":
+            return
+
+        precio = validar_campo(
+            "Ingrese el precio del vehículo (00 para cancelar): ",
+            validar_precio, "El valor del vehículo debe ser mayor a $6.880.000")
+        if precio == "cancelar":
+            return
+
+        multas = []
+        while True:
+            monto = int(
+                input("Ingrese el monto de la multa (0 para terminar de ingresar multas): "))
+            if monto == 0:
+                break
+            fecha = validar_campo("Ingrese la fecha de la multa (DD-MM-AAAA) (0 para cancelar): ",
+                                  validar_fecha, "Formato inválido. Ingrese una fecha en el formato (DD-MM-AAAA)")
+            if fecha == "cancelar":
+                return
+            multas.append({"monto": monto, "fecha": fecha})
+
+        fecha_registro = validar_campo(
+            "Ingrese la fecha del registro (DD-MM-AAAA) (00 para cancelar): ",
+            validar_fecha, "Formato inválido. Ingrese una fecha en el formato (DD-MM-AAAA)")
+        if fecha_registro == "cancelar":
+            return
+
+        nom_duenno = input(
+            "Ingrese nombre del dueño del vehículo (00 para cancelar): ")
+        if nom_duenno == "cancelar":
+            return
+
+        guardar(tipo, patente, marca, modelo, precio,
+                multas, fecha_registro, nom_duenno)
+        print(vehiculos)
+        break
+
+
 def buscar():
-    return
+    while True:
+        print("Ingrese una patente ('AA1111' o 'AAAA11') para consultar los datos de un vehículo o presione 00 para salir: ")
+        patente_buscada = input().upper().replace("-", "")
+        if patente_buscada == "00":
+            break
+
+        auto_encontrado = None
+        for auto in vehiculos:
+            if auto['patente'] == patente_buscada:
+                auto_encontrado = auto
+                break
+
+        if auto_encontrado:
+            # Imprimir los datos básicos del vehículo
+            print(f"Tipo de vehículo: {auto_encontrado['tipo']}")
+            print(f"Patente del vehículo: {auto_encontrado['patente']}")
+            print(f"Marca del vehículo: {auto_encontrado['marca']}")
+            print(f"Modelo del vehículo: {auto_encontrado['modelo']}")
+            print(f"Precio del vehículo: ${auto_encontrado['precio']}")
+            print(f"Fecha de registro: {auto_encontrado['fecha_registro']}")
+            print(f"Nombre dueño del vehículo: {
+                  auto_encontrado['nom_duenno']}")
+
+            # Si tiene multas, imprimirlas
+            if auto_encontrado["multas"]:
+                print("Multas del vehículo:")
+                for multa in auto_encontrado["multas"]:
+                    print(f"- Monto: {multa['monto']
+                                      }, Fecha: {multa['fecha']}")
+            else:
+                print("El vehículo no tiene multas registradas.")
+        else:
+            print("No se encontró ningún vehículo con esa patente.")
 
 
 def imprimir_certificados():
--- Principal.py
+++ Principal.py
@@ -2,46 +2,13 @@
 
 import Funciones
 
-
-def menu():
-    '''Definiendo la función que mostrará el menú'''
-    print("Menú Automotora Ruta 40 (Región de Los Lagos)")
-    print("1. Registrar vehículo")
-    print("2. Buscar vehículo")
-    print("3. Impresión de certificados")
-    print("4. Salir")
-
-
 while True:
-    menu()
+    Funciones.menu()
     print("Ingrese una opción del menú")
     opcion = int(input())
 
     if opcion == 1:
-        while True:
-            tipo = Funciones.validar_campo(
-                "Ingrese el tipo de vehículo 'Auto', 'Suvs', 'Pickups', 'Vans': ", Funciones.validar_tipo, "Tipo de vehículo inválido. Debe ser Auto, Suvs, Pickups o Vans.")
-            patente = Funciones.validar_campo(
-                "Ingrese la patente del vehículo debe contener 2 letras y 4 números ('AA1111') o 4 letras y 2 números ('AAAA11'): ", Funciones.validar_patente, "Formato de patente inválido debe ser 'AA1111' o 'AAAA11'")
-            marca = Funciones.validar_campo(
-                "Ingrese la marca del vehículo: ", Funciones.validar_marca, "La marca debe contener entre 2 y 12 caractéres")
-            modelo = Funciones.validar_campo("Ingrese el modelo del vehículo: ", Funciones.validar_modelo,
-                                             "No es posible dejar el campo vacío. Por favor ingrese un valor")
-            precio = Funciones.validar_campo(
-                "Ingrese el precio del vehículo: ", Funciones.validar_precio, "El valor del vehículo debe ser mayor a $6.880.000")
-            multas = []
-            while True:
-                monto = int(
-                    input("Ingrese el monto de la multa (ingrese 0 en caso de no existir multa): "))
-                if monto == 0:
-                    break
-                fecha = input("Ingrese la fecha de la multa (DD-MM-AAAA): ")
-                multas.append({"monto": monto, "fecha": fecha})
-            fecha_registro = Funciones.validar_campo(
-                "Ingrese la fecha de la multa (DD-MM-AAAA): ", Funciones.validar_fecha, "formato inválido. Ingrese una fecha en el formato (DD-MM-AAAA)")
-            nom_duenno = input("Ingrese nombre del dueño del vehículo: ")
-            Funciones.guardar(tipo, patente, marca, modelo, precio,
-                              multas, fecha_registro, nom_duenno)
+        Funciones.registrar_vehiculo()
     elif opcion == 2:
         Funciones.buscar()
     elif opcion == 3:
